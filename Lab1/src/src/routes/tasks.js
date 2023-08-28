de4538ceb9dfe9edaa510fba896466943a152eb7
dbellol <dbellol@unal.edu.co>
2023-08-28 07:58:20 -0500

    Subida real

--- Lab1/src/src/routes/tasks.js
+++
@@ -1,7 +0,0 @@
-const express =require('express');
-const router = express.Router();
-const customerController=require('../controllers/taskController');
-
-router.get('/', customerController.list)
-router.post('/add', customerController.save)
-module.exports = router;
